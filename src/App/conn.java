4f47bb694f67ab8ac67303c9da22f044cfb86de6
faisalahmedsifat <Faisalahmed531@gmail.com>
2020-12-06 16:11:48 +0600

    added jdbcdriver to the project and added connection class

---
+++ src/App/conn.java
@@ -0,0 +1,24 @@
+package App;
+
+import java.sql.*;
+
+public class conn {
+    public Connection c;
+    public Statement s;
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            c = DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12379786", "sql12379786", "212d6wjMn2");
+            s = c.createStatement();
+            //Debugging
+//            System.out.println("Connection Established");
+        } catch (ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //Debugging Option
+    /*public static void main(String[] args) {
+        new conn();
+    }*/
+}
